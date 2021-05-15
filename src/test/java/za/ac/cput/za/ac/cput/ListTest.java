dfaf2cc762659a7433c6420ac1306a4571d33280
2172105135 <81383559+2172105135@users.noreply.github.com>
2021-05-15 21:52:32 +0200

    The List Test program now contains The add and set test collection interfaces.

--- src/test/java/za/ac/cput/za/ac/cput/ListTest.java
+++ src/test/java/za/ac/cput/za/ac/cput/ListTest.java
@@ -1,6 +1,43 @@
 package za.ac.cput.za.ac.cput;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // This test class is assigned to a new branch called 217205135
 public class ListTest {
+    List<Double> price = new ArrayList();
+
+    @Test
+    void listAdd(){
+      price.add(20.00);
+      price.add(150.00);
+      price.add(80.00);
+
+        System.out.println(price);
+        price.add(1,70.00);
+
+        System.out.println(price);
+
+    }
+
+    @Test
+    void ListRemove(){
+        price.add(0,20.00);
+        price.add(1,150.00);
+        price.add(2,80.00);
+
+        System.out.println(price);
+        price.add(1,70.00);
+
+        System.out.println(price);
+
+        price.remove(150.00);
+
+        System.out.println("Removing the index 2 element of the current listing:"  + price);
+
+    }
+
 
 }
