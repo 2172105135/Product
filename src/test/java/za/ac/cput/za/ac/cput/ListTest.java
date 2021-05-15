ee660c16d395a2342c74fd25ce92af3d88042286
2172105135 <81383559+2172105135@users.noreply.github.com>
2021-05-15 22:27:06 +0200

    The final test which is  to find element(s).

--- src/test/java/za/ac/cput/za/ac/cput/ListTest.java
+++ src/test/java/za/ac/cput/za/ac/cput/ListTest.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // This test class is assigned to a new branch called 217205135
 public class ListTest {
-    List<Double> price = new ArrayList();
+    List price = new ArrayList();
 
     @Test
     void listAdd(){
-      price.add(20.00);
-      price.add(150.00);
-      price.add(80.00);
+        price.add(0,20.00);
+        price.add(1,150.00);
+        price.add(2,80.00);
 
         System.out.println(price);
         price.add(1,70.00);
@@ -36,6 +38,20 @@ public class ListTest {
         price.remove(150.00);
 
         System.out.println("Removing the index 2 element of the current listing:"  + price);
+
+    }
+    @Test
+    void ListFind(){
+        price.add(0,20.00);
+        price.add(1,150.00);
+        price.add(2,80.00);
+        System.out.println(price);
+
+        System.out.println("The program is finding a price in the 0 index: "+price.get((0)));
+        assertEquals(3,price.size());
+
+
+
 
     }
 
